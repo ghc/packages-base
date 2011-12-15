51550253fd7ec522fdcc71e391837a6688bda6e2
Ian Lynagh <igloo@earth.li>
2011-12-15 20:23:39 +0000

    Fix typo

--- Debug/Trace.hs
+++ Debug/Trace.hs
@@ -51,7 +51,7 @@ import System.IO (hPutStrLn,stderr)
 -- $tracing
 --
 -- The 'trace', 'traceShow' and 'traceIO' functions print messages to an output
--- stream. They are intended for \"prinf debugging\", that is: tracing the flow
+-- stream. They are intended for \"printf debugging\", that is: tracing the flow
 -- of execution and printing interesting values.
 
 -- The usual output stream is 'System.IO.stderr'. For Windows GUI applications
